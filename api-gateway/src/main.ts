7e2f63f968e930a0b7a45e160285fcb84207d936
KuelloKompleteEdition <159363862+KuelloKompleteEdition@users.noreply.github.com>
2025-04-05 22:41:47 -0500

    Agregar Filters
    Autor autorisimo: Cuello
    PD: No sirven
    
    Co-Authored-By: Juanestebane926 <105894607+Juanestebane926@users.noreply.github.com>

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
-
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 async function bootstrap() {
   const logger = new Logger('Path-Gateway');
   const app = await NestFactory.create(AppModule);
 
-  
+  app.enableCors({
+    origin: true,
+    credentials: true
+  });
   await app.listen(envs.PORT);
 
   logger.log(`Path Gateway is running on port ${envs.PORT}`);
